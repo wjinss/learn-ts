121fc4a9b9c055d9f596f6c042c3bf80578a0a4d
wjinss <aastu4343@gmail.com>
2025-08-08 19:36:11 +0900

    edu : unknown 타입

--- 2-basic-types/2.5-unknown/2.5.1-examples/example.ts
+++ 2-basic-types/2.5-unknown/2.5.1-examples/example.ts
@@ -1,24 +1,23 @@
 // 반환 타입을 any 대신, unknown으로 지정
 async function getUserAge(id: number): Promise<unknown> {
-  const response = await fetch(`https://dummyjson.com/users/${id}`)
-  const user = await response.json()
-  return user.age
+  const response = await fetch(`https://dummyjson.com/users/${id}`);
+  const user = await response.json();
+  return user.age;
 }
 
 function isOldEnoughToVote(age: number): boolean {
-  return age >= 18
+  return age >= 18;
 }
 
 async function process() {
-  const age = await getUserAge(1)
+  const age = await getUserAge(1);
   //    ^?
-  
+
   // 사용하기 전에 반드시 타입을 좁혀야 함
   // Argument of type 'unknown' is not assignable to parameter of type 'number'.
-  console.log(isOldEnoughToVote(age))
-  
+  if (typeof age === "number") console.log(isOldEnoughToVote(age));
 }
 
-;(async () => {
-  await process()
-})()
+(async () => {
+  await process();
+})();
--- 2-basic-types/2.5-unknown/2.5.2-exercise/exercise.ts
+++ 2-basic-types/2.5-unknown/2.5.2-exercise/exercise.ts
@@ -3,12 +3,17 @@
 // --------------------------------------------------------------------------
 
 // 1. `unknown` 타입의 변수를 선언하고 값을 할당하세요.
-
+let unknownType: string = "언노운";
+//   ^?
 
 // 2. `unknown` 타입의 인자를 받아서, 그 인자가 문자열이면
 //    true를 반환하는 `isString` 함수를 작성하세요.
 
+const isString = (value: unknown) => {
+  if (typeof value === "string") true;
+};
 
-// 3. 위에서 선언한 `unknownValue`가 문자열인지 
-//    `isString` 함수를 통해 확인하고 만약 문자열이라면, 
+// 3. 위에서 선언한 `unknownValue`가 문자열인지
+//    `isString` 함수를 통해 확인하고 만약 문자열이라면,
 //    그 길이를 콘솔에 출력하세요.
+isString(unknownType.length);
