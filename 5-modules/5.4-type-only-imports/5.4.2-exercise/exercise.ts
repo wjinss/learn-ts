277fa6323ce371b4c54aab7631bd8f8eefed7a1e
wjinss <aastu4343@gmail.com>
2025-08-10 01:46:27 +0900

    edu : 타입 전용 불러오기 구문

--- 5-modules/5.4-type-only-imports/5.4.2-exercise/exercise.ts
+++ 5-modules/5.4-type-only-imports/5.4.2-exercise/exercise.ts
@@ -3,8 +3,8 @@
 // --------------------------------------------------------------------------
 
 // 아래 import를 타입 전용(type-only) import로 변경하세요.
-import { Product } from '../types'
+import { type Product } from "../types";
 
 function getProductPrice(product: Product): number {
-  return product.price
+  return product.price;
 }
