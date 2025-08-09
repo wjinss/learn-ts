5daef6c7b8a46ace6e0d139da0cad90be0bd5fb0
wjinss <aastu4343@gmail.com>
2025-08-09 21:20:57 +0900

    edu : Record 유틸리티 타입

--- 3-typing-fundamentals/3.10-record/3.10.2-exercise/exercise.ts
+++ 3-typing-fundamentals/3.10-record/3.10.2-exercise/exercise.ts
@@ -5,17 +5,23 @@
 // 문제 1
 // 키(key)는 사용자 ID(문자열), 값(value)은 역할(문자열)인 Record 타입 `UserRoles`를 만드세요.
 
+type UserRoles = Record<string, string>;
 
 // 문제 2
 // `UserRoles` 타입을 사용해서 `roles`라는 변수를 만들고, 아래와 같이 역할을 할당하세요.
 // - ID가 'user1'인 사용자는 'admin' 역할
 // - ID가 'user2'인 사용자는 'editor' 역할
 // - ID가 'user3'인 사용자는 'viewer' 역할
-
+const roles: UserRoles = { user1: "admin", user2: "editor", user3: "viewer" };
+console.log(roles);
 
 // 문제 3
 // `getUserRole`이라는 함수를 만드세요.
 // 이 함수는 사용자 ID(문자열)와 `UserRoles` 객체를 받아서 해당 사용자의 역할을 반환합니다.
 
-// getUserRole 함수 테스트
+function getUserRole(ID: string, roles: UserRoles) {
+  return roles[ID];
+}
 
+// getUserRole 함수 테스트
+console.log(getUserRole("user2", roles));
