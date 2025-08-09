34cc6b6ff48c8ae610557aa42c122abb5e003570
wjinss <aastu4343@gmail.com>
2025-08-09 22:11:40 +0900

    edu : Function  타입

--- 4-typing-functions/4.1-typing-functions-basics/4.1.2-exercise/exercise.ts
+++ 4-typing-functions/4.1-typing-functions-basics/4.1.2-exercise/exercise.ts
@@ -3,31 +3,47 @@
 // --------------------------------------------------------------------------
 
 // 1. isEven 함수가 `num`을 number 타입으로 받고, boolean을 반환하도록 타입을 지정하세요.
-function isEven(num) {
-  return num % 2 === 0
+function isEven(num: number): boolean {
+  return num % 2 === 0;
 }
 
 // 2. multiply 함수가 두 개의 number 타입 매개변수를 받아 곱셈 결과를 number로 반환하도록 타입을 지정하세요.
-const multiply = (a, b) => {
-  return a * b
-}
+const multiply = (a: number, b: number): number => {
+  return a * b;
+};
 
 // 3. logError 함수가 `message`를 string 타입으로 받고, 반환값이 없도록(void) 타입을 지정하세요.
-function logError(message) {
-  console.error(message)
+function logError(message: string): void {
+  console.error(message);
 }
 
 // 4. getLength 함수가 모든 타입의 요소를 담을 수 있는 배열(arr)을 받아, number를 반환하도록 타입을 지정하세요.
-function getLength(arr) {
-  return arr.length
+function getLength(arr: Array<any>): number {
+  return arr.length;
+}
+
+function getLength2(arr: any[]): number {
+  return arr.length;
 }
 
 // 5. createUser 함수가 `name`(string)과 `age`(number)를 받아, name과 age 속성을 가진 객체를 반환하도록 타입을 지정하세요.
-function createUser(name, age) {
-  return { name, age }
+function createUser(name: string, age: number): { name: string; age: number } {
+  return { name, age };
+}
+
+interface User {
+  name: string;
+  age: number;
+}
+
+function createUser2(name: string, age: number): User {
+  return { name, age };
 }
 
 // 6. calculateArea 함수 표현식이 `width`와 `height`를 number 타입으로 받아, number를 반환하도록 타입을 지정하세요.
-const calculateArea = function (width, height) {
-  return width * height
-}
+const calculateArea: (width: number, height: number) => number = function (
+  width,
+  height
+) {
+  return width * height;
+};
