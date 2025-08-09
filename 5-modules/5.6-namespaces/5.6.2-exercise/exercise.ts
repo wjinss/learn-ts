f3bbd5609e0b65b935ac93a25c9a51066068b465
wjinss <aastu4343@gmail.com>
2025-08-10 02:03:21 +0900

    edu : 네임스페이스

--- 5-modules/5.6-namespaces/5.6.2-exercise/exercise.ts
+++ 5-modules/5.6-namespaces/5.6.2-exercise/exercise.ts
@@ -5,10 +5,21 @@
 // 문제 1
 // 두 숫자를 받아 합을 반환하는 `add` 함수를 포함하는 `MathOperations` 네임스페이스를 만드세요.
 
+namespace MathOperations {
+  export const add: (a: number, b: number) => number = (a, b) => {
+    return a + b;
+  };
+}
+
+console.log(MathOperations.add(3, 8));
 
 // 문제 2
 // 두 숫자를 받아 차를 반환하는 `subtract` 함수를 `MathOperations` 네임스페이스에 추가하세요.
-
-
+namespace MathOperations {
+  export const subtract: (a: number, b: number) => number = (a, b) => {
+    return a - b;
+  };
+}
+console.log(MathOperations.subtract(3, 8));
 // 문제 3
 // `MathOperations` 네임스페이스를 사용하여 두 숫자를 더하고 빼서, 결과를 콘솔에 출력하세요.
